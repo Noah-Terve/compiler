d4fcf3a82b193575913fa44e5e97e01535660861
theQuarkBot <neil.powers314@gmail.com>
2023-10-14 22:00:38 -0400

    Added skeleton test runners

---
+++ ScannerParser/parser_tester.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Script to run all parser tests
+
+WAMPUS="wampus"
+
+SignalError() {
+    if [ $error -eq 0 ] ; then
+	echo "FAILED"
+	error=1
+    fi
+    echo "  $1"
+}
+
+# Compare <outfile> <reffile> <difffile>
+# Compares the outfile with reffile.  Differences, if any, written to difffile
+Compare() {
+    generatedfiles="$generatedfiles $3"
+    echo diff -b $1 $2 ">" $3 1>&2
+    diff -b "$1" "$2" > "$3" 2>&1 || {
+	SignalError "$1 differs"
+	echo "FAILED $1 differs from $2" 1>&2
+    }
+}
+
+# Run <args>
+# Report the command, run it, and report any errors
+Run() {
+    echo $* 1>&2
+    eval $* || {
+	SignalError "$1 failed on $*"
+	return 1
+    }
+}
