b4a1729d1ffb6624eb5753d1fcb280657573269f
Haijun Si <79031887+haijun12@users.noreply.github.com>
2023-11-22 18:04:58 -0500

    edited statements

--- ScannerParser/bin/semant.ml
+++ ScannerParser/bin/semant.ml
@@ -250,7 +250,8 @@ let check units =
       | Expr e -> 
           let (envs', se) = check_expr e envs in
           (envs', SExpr(se))
-      (* | If(p, b1, b2) -> SIf(check_bool_expr p, check_stmt b1, check_stmt b2)
+      (* | If(p, b1, b2) -> SIf(check_bool_expr p, check_stmt b1, check_stmt b2) *)
+      (*
       | For(e1, e2, e3, st) ->
           SFor(expr e1, check_bool_expr e2, expr e3, check_stmt st)
       | While(p, s) -> SWhile(check_bool_expr p, check_stmt s)
