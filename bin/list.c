9290f6596d6a81db55b8119e8a35fedca03109d8
Noah Tervalon <noahtervalon@gmail.com>
2023-12-15 20:11:32 -0500

    updating list.c to remove comments, actual final commit

--- bin/list.c
+++ bin/list.c
@@ -10,11 +10,6 @@
 #include "list.h"
 #include <string.h>
 
-// typedef struct node {
-//     void* data;
-//     struct node *next;
-// } node;
-
 bool list_empty(node **head) {
     return head == NULL || *head == NULL;
 }
@@ -156,32 +151,3 @@ char *string_concat(char *s1, char *s2) {
     *v = n; \
     v; \
 })
-
-// static int main() {
-//     node **head = malloc(sizeof(node *));
-//     *head = NULL;
-
-//     list_insert(head, 0, MVAL(1));
-//     list_insert(head, 1, MVAL(2));
-
-//     list_int_print(head);
-
-//     list_remove(head, 0);
-
-//     list_int_print(head);
-
-//     list_insert(head, 0, MVAL(3));
-//     list_insert(head, 0, MVAL(4));
-
-//     list_int_print(head);
-
-//     list_remove(head, 1);
-
-//     list_int_print(head);
-
-//     printf("Length: %d\n", list_length(head));
-//     printf("At 0: %d\n", *(int *)list_at(head, 0));
-//     printf("At 1: %d\n", *(int *)list_at(head, 1));
-
-//     return 0;
-// }
